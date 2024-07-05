7fa372e951ccfd0f7a046a72064a3890f40985f7
jasim khan <jasim.khan@streamstech.com>
2024-07-05 13:19:43 +0600

    llm starts with pandasai

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # llm
+
+```
+>conda create -n pandasai_mysql
+>conda activate pandasai_mysql
+>conda deactivate
+>pip install -r requirements.txt
+>streamlit run app.py
+```
+### e-commerce customer behavior dataset
+- Write the first five row
+- How many rows and columns are in the dataset?
+- What is the average of the Total Spend column?
+- Plot a bar chart of the Gender column.
+- Draw a pie plot of the city column.
+- Plot a histogram of the age column.
+- Draw a bar plot of average Age by Gender.
+- Draw a box plot of the Age column.
+
---
+++ pandasai_mysql/app.py
@@ -0,0 +1,34 @@
+from pandasai.llm.local_llm import LocalLLM
+import streamlit as st
+from pandasai.connectors import MySQLConnector
+from pandasai import SmartDataframe
+
+my_connector = MySQLConnector(
+  config={
+    "host":"localhost",
+    "port":3306,
+    "database":"customer_behavior",
+    "username":"root",
+    "password":"root",
+    "table":"customer"
+  }
+)
+
+model = LocalLLM(
+  api_base="http://localhost:11434/v1",
+  model="llama3"
+)
+
+df_connector = SmartDataframe(
+  my_connector,
+  config={"llm":model}
+)
+
+st.title("MySql with Lama-2")
+
+prompt = st.text_input("Enter your prompt:")
+
+if st.button("Generate"):
+  if prompt:
+    with st.spinner("Generating response..."):
+      st.write(df_connector.chat(prompt))
