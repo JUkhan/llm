66ab52f970646beef797a930e0deb901a42b44b6
jasim khan <jasim.khan@streamstech.com>
2024-07-15 08:00:03 +0600

    groq doc

--- pandasai_mysql/pages/counter.py
+++ pandasai_mysql/pages/counter.py
@@ -22,7 +22,7 @@ if "celsius" not in st.session_state:
     # set the initial default value of the slider widget
     st.session_state.celsius = 50.0
 
-st.slider(
+st.sidebar.slider(
     "Temperature in Celsius",
     min_value=-100.0,
     max_value=100.0,
--- pandasai_mysql/pages/groq.py
+++ pandasai_mysql/pages/groq.py
@@ -1,34 +1,48 @@
-
-from dotenv import load_dotenv
-import pandas as pd
-import streamlit as st
-from langchain_groq.chat_models import ChatGroq
 import os
-from pandasai import SmartDataframe
+from langchain.chains import LLMChain
+from langchain_core.prompts import (
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder
+)
+from langchain_core.messages import SystemMessage
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain_groq import ChatGroq
+import streamlit as st
 
-load_dotenv()
 
-llm = ChatGroq(
+
+
+st.title('Groq Chat App')
+
+st.sidebar.title('Change memory length')
+conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value=5)
+memory = ConversationBufferMemory(k=conversational_memory_length, memory_key='chat_history', return_messages=True)
+system_prompt = 'You are a friendly conversational chatbot'
+
+st.write(os.environ['GROQ_API_KEY'])
+groq_chat = ChatGroq(
   model='mixtral-8x7b-32768',
   api_key=os.environ['GROQ_API_KEY']
 )
 
-st.title('Data visualize PandasAI with groq')
+user_question = st.text_area('Ask a question...')
 
-file_upload= st.file_uploader('Upload file', type='csv')
 
-if file_upload is not None:
-  data=pd.read_csv(file_upload)
-  st.write(data.head(3))
-
-  df=SmartDataframe(data, config={'llm':llm})
-  prompt = st.text_input('Enter your prompt:')
-  if st.button('Generate:'):
-    if prompt:
-      with st.spinner('Generating response'):
-        response = df.chat(prompt)
-        if isinstance(response, str):
-          st.write(response)
-        else:
-          st.json(response)
-          st.write(response.choices[0].delta.content)
+if st.button('Generate'):
+  if user_question:
+    with st.spinner("Generating response..."):
+      prompt = ChatPromptTemplate.from_messages([
+        SystemMessage(content=system_prompt),
+        MessagesPlaceholder(variable_name='chat_history'),
+        HumanMessagePromptTemplate.from_template('{human_input}')
+      ])
+      conversation = LLMChain(
+        llm = groq_chat,
+        prompt = prompt,
+        verbose = False,
+        memory = memory
+      )
+      response = conversation.predict(human_input = user_question)
+      
+      st.write('Chatbot', response)
